2c515ae34b6dbdbe8e248a42b7485d8bb86ae3dd
Nimira43 <n.rabone72@aol.com>
2025-02-14 01:11:01 +0000

    Cleaned up Vite's generated React App. Kept files from archive that'll still be useful.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <div>
+      <h1>Amphora</h1>
+    </div>
+  )
+}
+
+export default App
+
+// import { Container } from 'react-bootstrap'
+// import { Outlet } from 'react-router-dom'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header />
+//       <main className='py-3'>
+//         <Container>
+//           <Outlet />
+//         </Container>  
+//       </main>
+//       <Footer />
+//     </>
+//   )
+// }
+
+// export default App

3a845a698d6c8610ad6a2d985881da512828103d
Nimira43 <n.rabone72@aol.com>
2025-04-30 10:19:47 +0100

    Added Readonly type

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,13 +3,14 @@ import Footer from '../components/Footer'
 
 export default function StoreLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>)
+ {
   return (
     <div className='min-h-screen flex flex-col'>
       <Navigation />
-      <main>
+      <main className='flex-grow'>
         {children}
       </main>
       <Footer />

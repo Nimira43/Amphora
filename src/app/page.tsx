d48e3f05ef2ef241e240b927bff21110a1d519a5
Nimira43 <n.rabone72@aol.com>
2025-04-30 10:08:42 +0100

    Imprted Navigation and Footer into HomePage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
+
 export default function HomePage() {
   return (
     <div className='min-h-screen flex flex-col'>      
+      <Navigation />
+      <Footer />
     </div>
   )
 }

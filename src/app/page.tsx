72e54577c3410fb75ad7cda46dc36ab478d10563
Nimira43 <n.rabone72@aol.com>
2025-04-30 06:51:28 +0100

    Testing icons - all good.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { LuAmphora } from 'react-icons/lu'
+
 export default function Home() {
   return (
-    <div className='p-6 bg-dark'>
+    <div className='p-6 bg-dark flex gap-2'>
+      <LuAmphora className='text-prime text-3xl' />
       <h1 className='logo text-3xl text-prime'>Amphora</h1>
+      
     </div>
   )
 }

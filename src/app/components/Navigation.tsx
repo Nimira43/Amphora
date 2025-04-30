a5664eb6402181e24efb269e5895005eb9c84d02
Nimira43 <n.rabone72@aol.com>
2025-04-30 10:32:59 +0100

    Setting up navbar

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,5 +1,11 @@
 export default function Navigation() {
   return (
-    <div>Navigation</div>
+    <nav className='fixed top-0 left-0 right-0 bg-dark shdaow-md py-4'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='flex justify-between items-center'>
+
+        </div>
+      </div>
+    </nav>
   )
 }

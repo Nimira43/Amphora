702403e87ee0ea765c7ccb0b4982f2493f0adfed
Nimira43 <n.rabone72@aol.com>
2025-04-30 10:52:41 +0100

    Styled logo text

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -11,6 +11,7 @@ export default function Navigation() {
               <div className='w-9 h-9 bg-prime text-light rounded-full flex items-center justify-center text-2xl mr-2'>
                 <LuAmphora />
               </div>
+              <span className='logo text-prime text-2xl flex pt-1'>Amphora</span>
             </div>
           </Link>
         </div>

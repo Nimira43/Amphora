be4c98a49bb909a7a1ffe009834bdf9770eed8d4
Nimira43 <n.rabone72@aol.com>
2025-04-30 11:31:54 +0100

    Search bar styled

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -8,12 +8,27 @@ export default function Navigation() {
         <div className='flex justify-between items-center'>
           <Link href='/' className='flex items-center'>
             <div className='flex items-center'>
-              <div className='w-9 h-9 bg-prime text-light rounded-full flex items-center justify-center text-2xl mr-2'>
+              <div
+                className='w-9 h-9 bg-prime text-light rounded-full flex items-center justify-center text-2xl mr-2'
+              >
                 <LuAmphora />
               </div>
-              <span className='logo text-prime text-2xl flex pt-1'>Amphora</span>
+              <span
+                className='logo text-prime text-2xl flex pt-1'
+              >
+                Amphora
+              </span>
             </div>
           </Link>
+          <div className='hidden md:block flex-grow max-w-lg mx-8'>
+            <form className='group'>
+              <input
+                type='text'
+                placeholder='Search...'
+                className='w-full bg-prime rounded-md  text-light placeholder:text-light:70 focus:outline-0 py-2 pl-4 pr-10'
+              />
+            </form>
+          </div>
         </div>
       </div>
     </nav>

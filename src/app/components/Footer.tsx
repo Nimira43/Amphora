567b804d2e9e431f7483decdff53d7de56055314
Nimira43 <n.rabone72@aol.com>
2025-05-20 00:52:08 +0100

    Newsletter

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,13 @@ export default function Footer() {
   return (
     <footer>
       <div className='bg-dark py-16'>
-        <div className='max-w-7xl max-auto px-6'></div>
+        <div className='max-w-7xl max-auto px-6'>
+          <div className='flex flex-col md:flex-row items-center justify-between'>
+            <div className='mb-8 md:mb-0 md:max-w-xl'>
+              <h2 className='text-3xl font-medium text-light mb-2'>Signup to our Newsletter</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   )

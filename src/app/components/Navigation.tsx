d674f5ef6c874362ab36e764d562e9f78f91c040
Nimira43 <n.rabone72@aol.com>
2025-05-20 02:06:27 +0100

    Made big changes to Navigation CSS. Added custome button tranistion in global.css

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -7,7 +7,7 @@ import { HiOutlineUser } from 'react-icons/hi2'
 
 export default function Navigation() {
   return (
-    <nav className='fixed top-0 left-0 right-0 bg-dark shadow-md py-4'>
+    <nav className='fixed top-0 left-0 right-0 bg-grey-dark shadow-md py-4'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='flex justify-between items-center'>
           <Link href='/' className='flex items-center'>
@@ -29,11 +29,11 @@ export default function Navigation() {
               <input
                 type='text'
                 placeholder='Search...'
-                className='w-full bg-prime rounded-md  text-light placeholder:text-white/40 focus:outline-0 py-2 pl-4 pr-10'
+                className='w-full bg-light rounded-md  text-dark placeholder:grey-medium focus:outline-0 py-2 pl-4 pr-10'
               />
               <button
                 type='submit'
-                className='absolute right-1 top-1/2 -translate-y-1/2 text-white/40 hover:text-white p-1.5 rounded-full transition-colors cursor-pointer' 
+                className='absolute right-1 top-1/2 -translate-y-1/2 text-dark hover:text-prime p-1.5 rounded-md cursor-pointer' 
               >
                 <IoSearchOutline className='text-xl' />
               </button>
@@ -41,7 +41,7 @@ export default function Navigation() {
           </div>
           <div className='hidden md:flex items-center space-x-4'>
             <div className='group'>
-              <button className='text-white/70 hover:text-white bg-prime transition-colors p-2 rounded-md flex items-center cursor-pointer'>
+              <button className='text-light bg-prime hover:bg-prime-dark p-2 rounded-md flex items-center cursor-pointer'>
                 <LiaStoreAltSolid className='text-2xl mr-1' />
                 <span
                   className='hidden md:inline text font-medium uppercase'
@@ -52,13 +52,13 @@ export default function Navigation() {
             </div>
             <Link
               href='/cart'
-              className='text-white/70 hover:text-white bg-prime flex justify-center items-center p-2 rounded-md transition-colors'
+              className='text-light bg-prime hover:bg-prime-dark flex justify-center items-center p-2 rounded-md'
             >
               <BsCart3 className='text-2xl' />
             </Link>
             <Link
               href='/login'
-              className='text-white/70 hover:text-white bg-prime flex justify-center items-center p-2 rounded-md transition-colors'
+              className='text-light bg-prime hover:bg-prime-dark flex justify-center items-center p-2 rounded-md'
             >
               <HiOutlineUser className='text-2xl' />
             </Link>

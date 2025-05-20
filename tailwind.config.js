269560b527a21f95b7a42af627a11a1a3ef78eb6
Nimira43 <n.rabone72@aol.com>
2025-05-20 01:41:07 +0100

    Updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'prime': '#ff4500',
+        'prime-dark': '#de3b00',
+        'prime-light': '#ff5f34',
+        'support': '#008080',
+        'dark':'#111',
+        'light': '#fffcfa',
+        'grey-dark': '#333',
+        'grey-medium': '#999',
+        'grey-light': '#ccc',
+        'grey-light-extra': '#eee'
+      }
+    },
   },
   plugins: [],
 }

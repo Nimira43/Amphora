2963de70d1c11738b07f49ffb20ad3580b31023c
Nimira43 <n.rabone72@aol.com>
2025-08-08 00:40:06 +0100

    ProductsPage - 3

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -5,7 +5,9 @@ export default function ProductsPage() {
     <div
       className='max-w-7xl mx-auto px-4 pt-24 mb-16'
     >
-      
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+        
+      </div>
     </div>
   )
 }

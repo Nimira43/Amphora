d3529c566f4e0e6b01f5add770e3057598697023
Nimira43 <n.rabone72@aol.com>
2025-09-15 13:33:10 +0100

    CategoryPage styling - 3. Tweaks made elsewhere to styling.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
         <div className='max-w-7xl mx-auto px-6'>
           <div className='flex flex-col md:flex-row items-center justify-between'>
             <div className='mb-8 md:mb-0 md:max-w-xl'>
-              <h2 className='text-3xl font-normal text-light mb-2'>Signup to our Newsletter</h2>
+              <h2 className='text-3xl font-medium text-light mb-2'>Signup to our Newsletter</h2>
               <p className='text-light font-light'>Packed with news and offers plus news about upcoming products.</p>
             </div>
             <div className='w-full md:w-auto'>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import './globals.css'
 
 const poppins = Poppins({
   subsets: ['latin'],
-  variable: '--font-poppins',
-  display: 'swap',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
 

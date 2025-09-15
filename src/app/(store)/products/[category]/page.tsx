53b67ea449e07da95357f6d2e819cfcff0987467
Nimira43 <n.rabone72@aol.com>
2025-09-15 13:05:01 +0100

    Importing Product and Category types

---
+++ src/app/(store)/products/[category]/page.tsx
@@ -0,0 +1,8 @@
+import { Product } from '@/app/data/products'
+import { Category } from '@/app/data/products'
+
+export default function CategoryPage() {
+  return (
+    <div>Category Page</div>
+  )
+}

77b455eb3ab8ed512d307aa43ab8e49eec75bafa
Nimira43 <n.rabone72@aol.com>
2025-09-30 08:59:42 +0100

    ProductCard - 2

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 export default function ProductCard() {
   return (
-    <div className='bg-light rounded-xl overflow-hidden shadow-md hover:shadow-xl transition-effect group'>Product Card Component</div>
+    <div className='bg-light rounded-xl overflow-hidden shadow-md hover:shadow-xl transition-effect group'>
+      <div className='h-64 relative bg-grey-light-extra p-6 flex items-center justify-center'>
+        <Image 
+          src='/images/product001.png'
+          alt='Product Image'
+          width={200}
+          height={200}
+        />
+      </div>
+    </div>
   )
 }

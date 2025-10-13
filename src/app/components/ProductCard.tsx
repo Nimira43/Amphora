3278a07df8d5155b1f1e0bfa30b031da364cec20
Nimira43 <n.rabone72@aol.com>
2025-10-14 00:13:19 +0100

    Passing props into the ProductCard component to make the content dynamic.

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -18,8 +18,8 @@ export default function ProductCard({
     <div className='bg-light rounded-xl overflow-hidden shadow-md hover:shadow-xl transition-effect group'>
       <div className='h-64 relative bg-grey-light-extra p-6 flex items-center justify-center'>
         <Image 
-          src='/images/product001.png'
-          alt='Product Image'
+          src={product.image}
+          alt={product.name}
           width={200}
           height={200}
           className='max-h-full object-contain h-full w-auto group-hover:scale-105 transition-transform duration-500'
@@ -27,21 +27,30 @@ export default function ProductCard({
       </div>
       <div className='p-6 border-t border-grey-light'>
         <h2 className='text-lg font-medium text-dark mb-2 line-clamp-1'>
-          Smartpone NT100
+          {product.name}
         </h2>
         <p className='text-sm text-grey-dark mb-4 line-clamp-2'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, minima.
+          {product.description}
         </p>
         <div className='flex items-center justify-between'>
-          <span className='text-xl font-medium text-prime'>£1,099</span>
+          <span 
+            className='text-xl font-medium'
+            style={{color: categoryColour}}
+          >
+            £{product.price.toLocaleString()}
+          </span>
           <div className='flex gap-2'>
             <Link 
               href='/products/smartphones'
-              className='bg-prime px-4 py-2 flex justify-center items-center rounded-md text-light text-sm uppercase font-medium'
+              className='px-4 py-2 flex justify-center items-center rounded-md text-light text-sm uppercase font-medium'
+              style={{backgroundColor: categoryColour}}
             >
               View Product
             </Link>
-            <button className='w-10 h-10 bg-prime rounded-md text-light flex items-center justify-center'>
+            <button 
+              className='w-10 h-10 rounded-md text-light flex items-center justify-center'
+              style={{backgroundColor: categoryColour}}
+            >
               <BsCart3 className='text-2xl' />
             </button>
           </div>

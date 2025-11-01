f3c6fe3f7e31f5fdf3cf2e59d98a7279156038ef
Nimira43 <n.rabone72@aol.com>
2025-11-01 04:41:26 +0000

    PRoduct image brough in - hard-coded at the moment

--- src/app/(store)/products/[category]/[productId]/page.tsx
+++ src/app/(store)/products/[category]/[productId]/page.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
+
 export default function ProductPage() {
   return (
     <div className='max-w-7xl mx-auto px-4 py-12'>
       <div className='flex flex-col md:flex-row bg-light rounded-2xl overflow-hidden shadow-lg'>
         <div className='md:w-1/2 bg-grey-light-extra p-8 flex items-center justify-center relative'>
           <div className='relative w-full h-[300px] md:h-[400px]'>
-            
+            <Image  
+              src='/images/product001.png'
+              alt='Product Image'
+              fill
+              priority
+              sizes='(max-width: 768px) 100vw, 50vw'
+            />
           </div>
         </div>
       </div>

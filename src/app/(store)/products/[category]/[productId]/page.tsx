7b3dc09dcab28a9fa2bb2713376461da2eb0e47f
Nimira43 <n.rabone72@aol.com>
2025-11-01 04:37:30 +0000

    Setting up the product card

--- src/app/(store)/products/[category]/[productId]/page.tsx
+++ src/app/(store)/products/[category]/[productId]/page.tsx
@@ -1,5 +1,13 @@
 export default function ProductPage() {
   return (
-    <div>ProductPage</div>
+    <div className='max-w-7xl mx-auto px-4 py-12'>
+      <div className='flex flex-col md:flex-row bg-light rounded-2xl overflow-hidden shadow-lg'>
+        <div className='md:w-1/2 bg-grey-light-extra p-8 flex items-center justify-center relative'>
+          <div className='relative w-full h-[300px] md:h-[400px]'>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }

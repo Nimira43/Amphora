bd804dc676853d57e3569021352b343f7d396d3a
Nimira43 <n.rabone72@aol.com>
2025-12-06 10:24:43 +0000

    Created new flexbox classes

--- src/app/(store)/products/[category]/[productId]/page.tsx
+++ src/app/(store)/products/[category]/[productId]/page.tsx
@@ -5,7 +5,7 @@ export default function ProductPage() {
   return (
     <div className='max-w-7xl mx-auto px-4 py-12'>
       <div className='flex flex-col md:flex-row bg-light rounded-2xl overflow-hidden shadow-lg'>
-        <div className='md:w-1/2 bg-grey-light-extra p-8 flex items-center justify-center relative'>
+        <div className='md:w-1/2 bg-grey-light-extra p-8 centre relative'>
           <div className='relative w-full h-[300px] md:h-[400px]'>
             <Image  
               src='/images/product001.png'
